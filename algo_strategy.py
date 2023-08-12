878ef94021ee34dbc9300a4435256f8f21d8799c
Ben Le Jeune <me.benlejeune@gmail.com>
2023-08-12 16:04:04 +0100

    Now prints detected strategy

--- algo_strategy.py
+++ algo_strategy.py
@@ -67,21 +67,6 @@ class AlgoStrategy(gamelib.AlgoCore):
         self.repair_locations = []
 
         ###############################################################
-
-        # these are the "types of turns" that the algorithm can make
-        global ATTACK_SCOUTS_R, ATTACK_DEMOLISHERS_R, ATTACK_WAIT, ATTACK_SCOUTS_R_PREP, ATTACK_DEMOLISHERS_R_PREP, ATTACK_DEMOLISHERS_L, ATTACK_DEMOLISHERS_L_PREP, ATTACK_SCOUTS_L, ATTACK_SCOUTS_L_PREP
-        ATTACK_WAIT = 0
-        ATTACK_SCOUTS_R = 1
-        ATTACK_DEMOLISHERS_R = 2
-        ATTACK_SCOUTS_R_PREP = 3
-        ATTACK_DEMOLISHERS_R_PREP = 4
-        ATTACK_DEMOLISHERS_L = 5
-        ATTACK_DEMOLISHERS_L_PREP = 6
-        ATTACK_SCOUTS_L = 7
-        ATTACK_SCOUTS_L_PREP = 8
-
-        self.attack_type = ATTACK_WAIT
-
         # used for predicting enemy scout rushes
         self.enemy_scout_attack_threshold = None
         self.enemy_mp = 0
@@ -177,6 +162,10 @@ class AlgoStrategy(gamelib.AlgoCore):
         """
         Taking inspiration from some effective defences we've seen.
         """
+
+        # at the start of each turn, predict the strategy.
+        strategy = self.detect_enemy_strategies(game_state)
+        gamelib.debug_write(strategy)
 
         # on the first turn
         if game_state.turn_number == 0:
@@ -395,157 +384,6 @@ class AlgoStrategy(gamelib.AlgoCore):
             game_state.attempt_upgrade(support_location)
             self.support_due = None
 
-    # OUTDATED
-    def build_immediate_reactive_defences(self, game_state):
-        """
-        Several reactive defences for the algorithm to build based on where they were hit in the previous round.
-        This also updates the self.attacked_locations dictionary
-        """
-        # these are the possible locations to detect
-        right_corner_locations = [[27, 13], [26, 12], [25, 11], [24, 10]]
-        right_corner = "right_corner"
-
-        left_corner_locations = [[0, 13], [1, 12], [2, 11], [3, 10]]
-        left_corner = "left_corner"
-
-        funnel_locations = [[23, 9], [22, 8], [21, 7]]
-        funnel = "funnel"
-
-        scout_wall_entrance = [26, 13]
-
-        # a reactive defence to the right corner being attacked
-        for location in right_corner_locations:
-            if location in self.scored_on_locations:
-                gamelib.debug_write("Attacked in right corner!")
-                attacked_in_right_corner = True
-                self.attacked_locations[right_corner] += 1
-        if self.attacked_locations[right_corner] > 0:
-            gamelib.debug_write("Building right corner reactive defences")
-            # places additional turrets
-            extra_turret_locations = [[24, 12], [25, 13]]
-            game_state.attempt_spawn(TURRET, extra_turret_locations)
-
-            wall_locations = [[24, 13], [25, 13]]
-            if not self.attack_type == ATTACK_SCOUTS_R:
-                wall_locations.append(scout_wall_entrance)
-            game_state.attempt_spawn(WALL, wall_locations)
-
-            # attempts upgrades
-            game_state.attempt_upgrade(extra_turret_locations)
-            game_state.attempt_upgrade(wall_locations)
-
-        # a reactive defence to the funnel being attacked
-        for location in funnel_locations:
-            if location in self.scored_on_locations:
-                self.attacked_locations[right_corner] += 1
-                gamelib.debug_write("Attacked in funnel location!")
-        if self.attacked_locations[funnel] > 0:
-            gamelib.debug_write("Building funnel reactive defences")
-            turret_locations = [[23, 12], [20, 10], [24, 12], [22, 11]]
-            wall_locations = [[23, 13], [22, 12], [20, 12]]
-            game_state.attempt_spawn(TURRET, turret_locations)
-            game_state.attempt_spawn(WALL, wall_locations)
-            game_state.attempt_upgrade(TURRET, turret_locations)
-            game_state.attempt_upgrade(WALL, wall_locations)
-
-        # reactive defences to left side being
-        if self.attacked_locations[left_corner] > 0:
-            self.reinforce_left_side(game_state)
-
-
-
-    # OUTDATED
-    def maintain_defences(self, game_state):
-        gamelib.debug_write("maintaining defences")
-        # the type of attack we'll be performing
-        # todo: implement 'repairing' damaged structures, where we delete and instantly re-place next turn.
-        attack_type = self.attack_type
-
-        scout_wall_entrance_left = [1, 13]
-        scout_wall_entrance_right = [26, 13]
-        funnel_entrance = [21, 11]
-        demolisher_left_entrance = [4, 11]
-
-        # old defensive layout
-        # wall_locations = [[0, 13], [1, 13], [23, 13], [25, 13], [27, 13], [20, 12], [22, 12], [2, 11], [20, 11], [23, 11], [3, 10], [19, 10], [4, 9], [18, 9], [5, 8], [6, 8], [7, 8], [8, 8], [9, 8], [10, 8], [11, 8], [12, 8], [13, 8], [14, 8], [15, 8], [16, 8], [17, 8], [24, 13]]
-        # turret_locations = [[1, 12], [23, 12]]
-
-        """ -: basic defensive structure :- """
-
-        # new defensive layout
-        wall_locations = [[0, 13], [2, 13], [3, 13], [23, 13], [24, 13], [25, 13], [27, 13], [20, 12],
-                          [22, 12], [20, 11], [23, 11], [5, 10], [19, 10], [6, 9], [18, 9], [7, 8], [8, 8],
-                          [9, 8], [10, 8], [11, 8], [12, 8], [13, 8], [14, 8], [15, 8], [16, 8], [17, 8]]
-        turret_locations = [[3, 12], [23, 12]]
-
-        # we build certain structures if we are making a certain attack this turn
-        if attack_type == ATTACK_SCOUTS_R:
-            # we're making a scout attack!
-            # we want to block off the funnel
-            wall_locations.insert(0, funnel_entrance)
-        else:
-            # if we aren't making a scout attack, we can reinforce the wall of the 'gun'
-            wall_locations.insert(0, scout_wall_entrance_right)
-
-        if attack_type == ATTACK_SCOUTS_L:
-            wall_locations.insert(0, funnel_entrance)
-        else:
-            wall_locations.insert(0, scout_wall_entrance_left)
-
-        if attack_type == ATTACK_DEMOLISHERS_L:
-            # for now, we'll always send demolishers through their entrance
-            gamelib.debug_write("demolishers on the left!")
-        else:
-            wall_locations.insert(0, demolisher_left_entrance)
-
-        # we want to re-build our basic structures
-        game_state.attempt_spawn(WALL, wall_locations)
-        game_state.attempt_spawn(TURRET, turret_locations)
-
-        # upgrade the 'core' locations
-        wall_upgrade_location = [24, 13]
-        turret_upgrade_location = [[23, 12], [3, 12]]
-        game_state.attempt_upgrade([wall_upgrade_location, *turret_upgrade_location])
-
-        secondary_turret_location = [[20, 10], [3, 12]]
-        game_state.attempt_spawn(TURRET, secondary_turret_location)
-        game_state.attempt_upgrade(secondary_turret_location)
-
-        # support_locations = [[22, 9], [19, 9], [20, 8], [19, 8], [18, 8], [19, 7], [18, 7], [17, 7], [16, 7], [15, 7], [18, 6], [17, 6], [16, 6], [15, 6], [17, 5], [16, 5], [14, 7], [13, 7], [14, 6]]
-        support_locations = [[13, 7], [14, 7], [12, 7], [15, 7], [13, 6], [14, 6]]
-        # they prioritise upgraded supports over more supports
-        for location in support_locations:
-            game_state.attempt_spawn(SUPPORT, location)
-            game_state.attempt_upgrade(location)
-
-        # turret at [2, 12]
-
-        secondary_support_locations = [[12, 6], [15, 6], [16, 7], [17, 7], [18, 7]]
-        for location in secondary_support_locations:
-            game_state.attempt_spawn(SUPPORT, location)
-            game_state.attempt_upgrade(location)
-
-        """ -: removing the necessary walls for the attack during the prep phase :- """
-
-        # we remove the following locations depending on the attack type
-        if attack_type == ATTACK_SCOUTS_R_PREP:
-            # if we're attacking with scouts next turn, open up the wall
-            game_state.attempt_remove(scout_wall_entrance_right)
-        else:
-            # if the funnel has been closed, re-open it, since we aren't attacking with scouts next turn
-            game_state.attempt_remove(funnel_entrance)
-
-    # OUTDATED
-    def reinforce_left_side(self, game_state):
-        """
-        This reinforces the left side by building more walls and turrets
-        """
-        turret_locations = [[3, 12], [2, 12], [0, 13]]
-        wall_locations = [[0, 13], [1, 13], [2, 13], [3, 13]]
-
-        game_state.attempt_spawn(TURRET, turret_locations)
-        game_state.attempt_upgrade(turret_locations)
-        game_state.attempt_spawn(WALL, wall_locations)
 
     """
     --- PREDICTION & COUNTER-ATTACKS ---
@@ -585,26 +423,6 @@ class AlgoStrategy(gamelib.AlgoCore):
         interceptor_location = [7, 6]
         no_of_interceptors = 1 # todo: make this not always 1
         game_state.attempt_spawn(INTERCEPTOR, interceptor_location, no_of_interceptors)
-
-    # OUTDATED
-    def predict_enemy_left_attack(self, game_state):
-        """
-        This predicts if the enemy will launch an attack on the left side.
-        Check to see if any walls are being deleted to make way for attacking units.
-        """
-        # these are the locations we will check
-        enemy_left_wall_locations = [[0, 14], [1, 14], [2, 14], [3, 14]]
-        enemy_wall_marked_for_removal = False
-        for location in enemy_left_wall_locations:
-            if location in self.enemy_deletions:
-                enemy_wall_marked_for_removal = True
-
-        # if they're removing a wall, we assume they are going to attack here next turn.
-        return enemy_wall_marked_for_removal
-    # OUTDATED
-    def turn_1_interceptors(self, game_state):
-        interceptor_locations = [[5, 8], [22, 8]]
-        game_state.attempt_spawn(INTERCEPTOR, interceptor_locations)
 
     """
     --- POSITION ANALYSIS ---
@@ -740,123 +558,6 @@ class AlgoStrategy(gamelib.AlgoCore):
         scout_location = [20, 6]
         no_of_scouts = game_state.number_affordable(SCOUT)
         game_state.attempt_spawn(SCOUT, scout_location, no_of_scouts)
-
-    # OUTDATED
-    def determine_attack_type(self, game_state):
-        """
-        Returns either ATTACK_WAITING, ATTACK_SCOUTS, OR ATTACK_DEMOLISHERS
-        """
-        last_turn_action = self.attack_type
-
-        """ -: resolving a previously prepared attack :- """
-
-        # first, we look at our prepared actions.
-        # if we prepared to send demolishers last turn, then we'll fire them off this turn
-        if last_turn_action == ATTACK_DEMOLISHERS_R_PREP:
-            return ATTACK_DEMOLISHERS_R
-
-        # if we prepared to send scouts last turn, we'll fire them off this turn.
-        if last_turn_action == ATTACK_SCOUTS_R_PREP:
-            # we make sure all our walls have been re-built
-            # if not, we wait another turn
-            # necessary_walls = [[21, 11], [19, 10], [18, 9], [7, 8], [8, 8], [9, 8], [10, 8], [11, 8], [12, 8], [13, 8],
-            #                    [14, 8], [15, 8], [16, 8], [17, 8]]
-            # walls_present = True
-            # for location in necessary_walls:
-            #     wall = game_state.contains_stationary_unit(location)
-            #     if not wall:
-            #         return ATTACK_SCOUTS_R_PREP
-
-            # TODO: We should check to see if the scouts are sufficient. Check these scouts will actually deal
-            #  damage, otherwise we should wait another turn.
-            return ATTACK_SCOUTS_R
-
-        """ -: conditions for demolisher attacks :- """
-
-        # next, we check to see if we want to send demolishers.
-        # if we sent demolishers last turn and it worked, let's do that again!
-        if last_turn_action == ATTACK_DEMOLISHERS_R and len(self.score_locations) > 0:
-            gamelib.debug_write("Our demolishers worked last turn, so let's do them again!")
-            return ATTACK_DEMOLISHERS_R
-
-        # we simulate a demolisher attack to see how much damage it would deal
-        # if it would deal a lot, then we fire it.
-        demolisher_location = [25, 11]
-        demolisher_count = game_state.number_affordable(DEMOLISHER)
-        params = sim.make_simulation(game_state, game_state.game_map, None, DEMOLISHER, demolisher_location, 0, demolisher_count)
-        if params is not None:
-            # we've made a simulation, let's see how much damage it dealt
-            # todo: un-break the simulation so we can un-comment this
-            evaluation = sim.simulate(*params)
-            structure_points = evaluation.points_destroyed
-            # if we destroyed enough structure points worth, then we want to fire this demolisher next turn
-            gamelib.debug_write(f"Demolisher would deal {structure_points}")
-            if structure_points >= 8:
-                if last_turn_action == ATTACK_DEMOLISHERS_R or last_turn_action == ATTACK_DEMOLISHERS_R_PREP:
-                    return ATTACK_DEMOLISHERS_R
-                else:
-                    return ATTACK_DEMOLISHERS_R_PREP
-
-        """ -: conditions for scout attacks :- """
-
-        # now, we don't want to send demolishers.
-        # let's see if we want to make a scout attack.
-        # we want a minimum payload of 6 damaging scouts
-        no_of_scouts = 6
-        if self.strong_scout_attack_needed(game_state):
-            # if we need a strong attack we want 6 suicides
-            no_of_scouts += 6
-        else:
-            # if we only need a weak one, we'll use 2 scouts
-            no_of_scouts += 2
-        no_of_demolishers = self.num_demolishers_needed(game_state)
-        if game_state.project_future_MP(1) >= (game_state.type_cost(SCOUT)[MP] * no_of_scouts) + (game_state.type_cost(DEMOLISHER)[MP] * 0):
-            # if we can, we'll launch a scout attack next turn.
-            return ATTACK_SCOUTS_R_PREP
-
-        # if we've made it this far, none of these seem like particularly good options.
-        # we'll just wait this turn out and build up some more MP
-        return ATTACK_WAIT
-    # OUTDATED
-    def scout_rush_right(self, game_state):
-        """
-        We're performing a scout rush
-        """
-        # the number of scouts we can afford
-        scouts_affordable = game_state.number_affordable(SCOUT)
-        strong_attack_needed = self.strong_scout_attack_needed(game_state)
-
-        scout_suicide_location = [[14, 0]]
-        scout_payload_location = [[13, 0]]
-
-        no_of_suicide_scouts = 2
-
-        # we send the required number of suicide scouts
-        if strong_attack_needed:
-            # if we need a strong attack, we send 6 suicide scouts
-            no_of_suicide_scouts = 6
-
-        demolisher_location = [[22, 8]]
-        no_of_demolishers = self.num_demolishers_needed(game_state)
-
-        # spawn demolishers
-       #  game_state.attempt_spawn(DEMOLISHER, demolisher_location, no_of_demolishers)
-
-        # spawn suicide scouts
-        game_state.attempt_spawn(SCOUT, scout_suicide_location, no_of_suicide_scouts)
-
-        # now that we've prepared the suicide scouts, we send the payload scouts
-        game_state.attempt_spawn(SCOUT, scout_payload_location, scouts_affordable - no_of_suicide_scouts)
-    # OUTDATED
-    def demolisher_attack_right(self, game_state):
-        """
-        We're performing a demolisher attack
-        """
-        demolisher_location = [[25, 11]]
-        demolishers_affordable = game_state.number_affordable(DEMOLISHER)
-
-        # we simply spawn as many demolishers as we can
-        game_state.attempt_spawn(DEMOLISHER, demolisher_location, demolishers_affordable)
 
     def least_damage_spawn_location(self, game_state, location_options):
         """
